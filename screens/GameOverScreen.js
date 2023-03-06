7e72edaca933dec16cc8f01332530812e31c6fe4
my-cosmos <sangram1alt2acc3io@gmail.com>
2023-03-06 11:18:21 +0530

    Replacing DimensionsAPI with useWindowDimensions in StartGameScreen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,23 +1,30 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
 import PrimaryButton from "../components/ui/PrimaryButton";
 import { Title } from "../components/ui/Title";
 import Colors from "../constants/colors";
 
-export const GameOverScreen = ({roundsNumber, UserNumber, onStartNewGame}) => {
+export const GameOverScreen = ({
+  roundsNumber,
+  UserNumber,
+  onStartNewGame,
+}) => {
   return (
     <View style={styles.rootContainer}>
       <Title>Game Over!</Title>
       <View style={styles.imageContainer}>
         <Image style={styles.image} source={require("../assets/iso.jpg")} />
       </View>
-      <Text style={{fontSize: 24, textAlign: 'center', marginBottom: 24}}>
-        Your phone needed <Text style={styles.highlight}>{roundsNumber}</Text> rounds to
-        guess the number <Text style={styles.highlight}>{UserNumber}</Text>
+      <Text style={{ fontSize: 24, textAlign: "center", marginBottom: 24 }}>
+        Your phone needed <Text style={styles.highlight}>{roundsNumber}</Text>{" "}
+        rounds to guess the number{" "}
+        <Text style={styles.highlight}>{UserNumber}</Text>
       </Text>
       <PrimaryButton _onPress={onStartNewGame}>Start New Game</PrimaryButton>
     </View>
   );
 };
+
+const deviceWidth = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
   rootContainer: {
@@ -27,7 +34,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   imageContainer: {
-    borderRadius: 150,
+    width: deviceWidth < 380 ? 150 : 300,
+    height: deviceWidth < 380 ? 150 : 300,
+    borderRadius: deviceWidth < 380 ? 75 : 150,
     width: 300,
     height: 300,
     borderWidth: 3,
